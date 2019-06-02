d4fe62879da1da4962e91e83dc6147f010408bd6
Mateusz-Slisz <mateusz.slisz@skygate.io>
2019-06-02 19:28:20 +0200

    [task-05] fix signup and login, add base template for forms

--- signup.php
+++ signup.php
@@ -1,4 +1,4 @@
-<?php include 'base.php' ?>
+<?php include 'form-base.php' ?>
 
 <?php startblock('title') ?>
 Signup
@@ -19,24 +19,23 @@ $(document).ready(function() {
             <div class="card-panel z-depth-5">
                 <h4 class="center">Signup</h4>
                 <div class="row">
-                    <form class="col s12 m12">
+                    <form class="col s12 m12" method="POST" action="signup.php">
                         <div class="row">
                             <div class="input-field col s12 m12">
                                 <i class="material-icons prefix">account_circle</i>
-                                <input id="icon_prefix" type="text" class="validate">
+                                <input id="icon_prefix" type="text" name="username" class="validate" required>
                                 <label for="icon_prefix">Username</label>
                             </div>
 
                             <div class="input-field col s12 m12">
                                 <i class="material-icons prefix">lock</i>
-                                <input id="icon_password" type="password" class="validate">
+                                <input id="icon_password" type="password" name="password" class="validate" required>
                                 <label for="icon_password">Password</label>
                             </div>
 
                             <div class="input-field col s12 m12">
                                 <i class="material-icons prefix">view_headline</i>
-                                <select>
-                                    <option value="" disabled selected>Choose your permission</option>
+                                <select name="permission" required>
                                     <option value="1">Silver</option>
                                     <option value="2">Gold</option>
                                     <option value="3">Diamond</option>
@@ -57,4 +56,49 @@ $(document).ready(function() {
         </div>
     </div>
 </div>
+
+<?php
+    $username = @$_POST["username"] ?: NULL;
+    $password = @$_POST["password"] ?: NULL;
+    $permission = @$_POST["permission"] ?: NULL;
+    $servername = "localhost";
+    $username_db = "root";
+    $password_db = "";
+    $dbname = "observer";
+    
+    if (isset($username, $password, $permission)) {
+        $conn = new mysqli($servername, $username_db, $password_db, $dbname);
+
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $select_sql = "
+        SELECT username FROM users WHERE username='$username'
+        ";
+        $result = $conn->query($select_sql);
+        
+        if ($result->num_rows > 0) {
+            echo "<script> M.toast({html: 'Username with given username exists!'})</script>";
+        }
+        else {
+            $hash = password_hash($password, PASSWORD_DEFAULT);
+        
+            $insert_sql = "
+            INSERT INTO users (username, password, permission_id) 
+            VALUES ('$username', '$hash', '$permission')
+            ";
+    
+            if ($conn->query($insert_sql) === TRUE) {
+                echo "<script> M.toast({html: 'You were signed up!'})</script>";
+            } else {
+                echo "$conn->error";
+            }
+        }
+    
+        $conn->close();
+        
+    }
+
+?>
 <?php endblock() ?>
